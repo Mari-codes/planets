34f272441b42f320c4eaa1aac0270f6e17c21646
Mariana <mariana.iwaivk@gmail.com>
2026-01-03 08:25:50 -0300

    feat(planets-nav): implement navigation links with planet-specific hover effects

--- src/components/PlanetsNav/index.tsx
+++ src/components/PlanetsNav/index.tsx
@@ -1,4 +1,5 @@
 import { planets } from '@/data/planets';
+import { Link } from 'react-router-dom';
 import styles from './PlanetsNav.module.scss';
 
 export const PlanetsNav = () => {
@@ -6,8 +7,15 @@ export const PlanetsNav = () => {
     <nav className={styles['planets-nav']}>
       <ul className={styles['planets-nav__list']}>
         {planets.map((planet) => (
-          <li key={planet.name} className={styles['planets-nav__item']}>
-            {planet.name.toUpperCase()}
+          <li
+            key={planet.name}
+            className={styles['planets-nav__item']}
+            style={{ ['--planet-color-name' as string]: planet.name.toLowerCase() }}
+          >
+            <Link to={`/${planet.name.toLowerCase()}`} className={styles['planets-nav__link']}>
+              <span className={styles['planets-nav__bullet']}></span>
+              {planet.name.toUpperCase()}
+            </Link>
           </li>
         ))}
       </ul>
